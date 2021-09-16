9a90777d402b8a373d0642c14f3f257a538bb4c4
freeman-lab <the.freeman.lab@gmail.com>
2021-09-15 22:17:05 -0700

    clean up dependencies

--- next.config.js
+++ next.config.js
@@ -10,12 +10,6 @@ const aliases = {
   '@utils': resolve('./utils'),
 }
 
-//// BUNDLE ANALYZER ////
-
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
-
 //// MDX ////
 
 const withMDX = require('@next/mdx')({
@@ -24,29 +18,27 @@ const withMDX = require('@next/mdx')({
 
 //// EXPORT ////
 
-module.exports = withMDX(
-  withBundleAnalyzer({
-    pageExtensions: ['jsx', 'js', 'md', 'mdx'],
-    webpack: (config, options) => {
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        ...aliases,
-      }
+module.exports = withMDX({
+  pageExtensions: ['jsx', 'js', 'md', 'mdx'],
+  webpack: (config, options) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      ...aliases,
+    }
 
-      config.resolve.alias['react'] = path.resolve(
-        __dirname,
-        '.',
-        'node_modules',
-        'react'
-      )
-      config.resolve.alias['theme-ui'] = path.resolve(
-        __dirname,
-        '.',
-        'node_modules',
-        'theme-ui'
-      )
+    config.resolve.alias['react'] = path.resolve(
+      __dirname,
+      '.',
+      'node_modules',
+      'react'
+    )
+    config.resolve.alias['theme-ui'] = path.resolve(
+      __dirname,
+      '.',
+      'node_modules',
+      'theme-ui'
+    )
 
-      return config
-    },
-  })
-)
+    return config
+  },
+})
