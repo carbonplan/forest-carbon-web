d7c4e9878887bf2f2d978ec461790a2b67bc3704
Kata Martin <katasm@gmail.com>
2021-09-30 11:44:27 -0700

    Update data

--- components/viewer/index.js
+++ components/viewer/index.js
@@ -10,13 +10,13 @@ const COLORMAPS = {
   'emissions-v1': { name: 'fire', reversed: false },
   'emissions-v0': { name: 'fire', reversed: false },
   'sinks-v1': { name: 'water', reversed: true },
-  'net-v1': { name: 'pinkgreen', reversed: false },
+  'net-v1': { name: 'pinkgreen', reversed: true },
 }
 
 const fallback = 'emissions-v1'
 
 const CLIMS = {
-  biomass: [0, 1000],
+  biomass: [0, 300],
   'emissions-v1': [0, 5000],
   'sinks-v1': [-5000, 0],
   'net-v1': [-30000, 30000],
@@ -61,7 +61,7 @@ function Viewer({ children, year, layer }) {
         mode={'dotgrid'}
         fillValue={9.969209968386869e36}
         source={
-          'https://carbonplan-climatetrace.s3.us-west-2.amazonaws.com/v1.2/map/forest_carbon_web_data_v1.pyr'
+          'https://carbonplan-climatetrace.s3.us-west-2.amazonaws.com/v1.2/map/forest_carbon_web_data_v2.pyr'
         }
         variable={'variable'}
         setRegionData={setRegionData}
@@ -69,6 +69,18 @@ function Viewer({ children, year, layer }) {
           year: Math.max(parseInt(year), 2015),
           band: layer || fallback,
         }}
+        frag={`
+        if (length(gl_PointCoord.xy - 0.5) > 0.5) {
+          discard;
+        }
+        if (variable == fillValue || variable == 3.4028234663852886e+38) {
+          discard;
+        }
+        float rescaled = (variable - clim.x)/(clim.y - clim.x);
+        vec4 c = texture2D(colormap, vec2(rescaled, 1.0));
+        gl_FragColor = vec4(c.x, c.y, c.z, opacity);
+        gl_FragColor.rgb *= gl_FragColor.a;
+        `}
       />
       <Enhancers />
       {children}
