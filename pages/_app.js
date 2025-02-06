c52638add5e44f88b1c7cf51ea8f6af55be0396a
Shane Loeffler <Shane98c@users.noreply.github.com>
2025-02-06 17:03:39 -0600

    use script

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import PlausibleProvider from 'next-plausible'
+import Script from 'next/script'
 import { ThemeProvider } from 'theme-ui'
 import { MDXProvider } from '@mdx-js/react'
 import '@carbonplan/components/fonts.css'
@@ -10,15 +10,21 @@ import { RegionProvider } from '../components/region'
 
 const App = ({ Component, pageProps }) => {
   return (
-    <PlausibleProvider domain='carbonplan.org'>
-      <ThemeProvider theme={theme}>
-        <MDXProvider>
-          <RegionProvider>
-            <Component {...pageProps} />
-          </RegionProvider>
-        </MDXProvider>
-      </ThemeProvider>
-    </PlausibleProvider>
+    <ThemeProvider theme={theme}>
+      {process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' && (
+        <Script
+          strategy='lazyOnload'
+          data-domain='carbonplan.org'
+          data-api='https://carbonplan.org/proxy/api/event'
+          src='https://carbonplan.org/js/script.js'
+        />
+      )}
+      <MDXProvider>
+        <RegionProvider>
+          <Component {...pageProps} />
+        </RegionProvider>
+      </MDXProvider>
+    </ThemeProvider>
   )
 }
 
